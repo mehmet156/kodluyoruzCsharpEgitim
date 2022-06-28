657d065119a86b2abea573eefefcc80987a000bf
mehmet156 <mehmetozden156@gmail.com>
2022-06-28 15:24:55 +0300

    operatorDeneme

--- kodluyoruzCsharp/Program.cs
+++ kodluyoruzCsharp/Program.cs
@@ -6,12 +6,29 @@ namespace kodluyoruzCsharp
     {
         static void Main(string[] args)
         {
-            string yirmi = "20";
-         int yirmiInt = 20;
-         string sonuc = null;
+            // string yirmi = "20";
+            // int yirmiInt = 20;
+            // string sonuc = null;
 
-        sonuc =yirmi+yirmiInt.ToString();
-            Console.WriteLine(sonuc);
+            //sonuc =yirmi+yirmiInt.ToString();
+            // Console.WriteLine(sonuc);
+            string sayi1 = null;
+            string sayi2 = null;
+
+            Console.WriteLine("sayi1 giriniz");
+            sayi1 = Console.ReadLine();
+            Console.WriteLine("sayi2 giriniz");
+            sayi2 = Console.ReadLine();
+            Convert.ToInt32(sayi1);
+
+            if (Convert.ToInt32(sayi1) <= Convert.ToInt32(sayi2))
+            {
+                Console.WriteLine("sayi1 küçük yada eşit");
+            }
+            else
+                Console.WriteLine("sayi1 büyük");
+
+
         }
     }
 }
