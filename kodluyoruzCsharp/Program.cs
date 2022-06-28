b4c711a3f87e9cc6c1c7645c891c50a73536b807
mehmet156 <mehmetozden156@gmail.com>
2022-06-28 14:45:56 +0300

    degiskenler

---
+++ kodluyoruzCsharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace kodluyoruzCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yirmi = "20";
+         int yirmiInt = 20;
+         string sonuc = null;
+
+        sonuc =yirmi+yirmiInt.ToString();
+            Console.WriteLine(sonuc);
+        }
+    }
+}
